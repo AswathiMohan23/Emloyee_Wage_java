77b7a5d106e24b5297b498fc48f0174204138ab2
AswathiMohan23 <achu999here@gmail.com>
2022-10-05 19:06:37 +0530

    ADDED: a code displaying welcome to employee wage commutation

---
+++ src/com/java/Welcome_Message.java
@@ -0,0 +1,8 @@
+package com.java;
+//Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
+public class Welcome_Message {
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+
+}
