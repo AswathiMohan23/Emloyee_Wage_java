7b79d8d773a893e3ca7e9e19a4166a1a7d6de64f
AswathiMohan23 <achu999here@gmail.com>
2022-10-05 19:57:44 +0530

    [ADDED]: a code to calculate part time employee wage

---
+++ src/com/java/UC3_PartTime_EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.java;
+
+//Add Part time Employee & Wage Assume Part time Hour is 8
+
+public class UC3_PartTime_EmployeeWage {
+    public static void main(String[] args){
+        int wage_per_hour=20;
+        int partTime=8;
+        System.out.println("Daily employee wage :  "+(wage_per_hour*partTime));
+    }
+}
